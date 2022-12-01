7ec5fc74fc47e5128d84772201f36688223a5958
George Arnall <george.arnall@thetrainline.com>
2022-12-01 12:42:51 +0000

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+const string filePath = @"/Users/georgearnall/source/AdventOfCode/Day1/input.txt";
+var lines = File.ReadLines(filePath, Encoding.UTF8);
+
+var elfCalories = new List<int>();
+
+var currentElfCalories = 0;
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elfCalories.Add(currentElfCalories);
+        currentElfCalories = 0;
+        continue;
+    }
+
+    currentElfCalories += int.Parse(line);
+}
+
+Console.WriteLine("Top Three Elves");
+var top3 = elfCalories.OrderByDescending(x => x).Take(3).ToArray();
+foreach (var calories in top3)
+{
+    Console.WriteLine($"Elf Calories {calories}");
+}
+Console.WriteLine($"Total: {top3.Sum()}");
