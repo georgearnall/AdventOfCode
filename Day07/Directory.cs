3f4e66d180760ff1bcd620147c18944d7097d697
George Arnall <george.arnall@thetrainline.com>
2022-12-08 10:47:26 +0000

    Renamed with leading zeros

--- Day07/Directory.cs
+++ Day07/Directory.cs
@@ -12,17 +12,16 @@ public class Directory
     public string Path { get; }
     public Directory? Parent { get; }
     public List<Directory> SubDirectories { get; } = new();
-    public int FileOnlySize { get; private set; }
+    public void AddFile(int size) => _fileOnlySize += size;
+    public void NewDirectory(string directoryName) => SubDirectories.Add(new Directory(directoryName, this));
     public int GetSize()
     {
         if (_size == 0)
         {
-            _size = SubDirectories.Sum(x => x.GetSize()) + FileOnlySize;
+            _size = SubDirectories.Sum(x => x.GetSize()) + _fileOnlySize;
         }
         return _size;
     }
-
-    private int _size = 0;
-    public void AddFile(int size) => FileOnlySize += size;
-    public void NewDirectory(string directoryName) => SubDirectories.Add(new Directory(directoryName, this));
+    private int _fileOnlySize;
+    private int _size;
 }
